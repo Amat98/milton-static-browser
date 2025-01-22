99ec3426a7acc592feb861e0356418d999767e1b
Amat98 <milton.yv98@gmail.com>
2025-01-22 13:39:30 -0500

    Colors and tittle

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ import { TypeAnimation } from "react-type-animation";
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex justify-center items-center">
-      <div className="text-5xl w-[150px]">
+      <div className="text-5xl w-[150px] mb-[100px]">
         <h1 className="">Hi</h1>
         <h1 className="">I am</h1>
         <TypeAnimation
           sequence={[
             // Same substring at the start will only be typed out once, initially
-            "Milton",
-            2000, // wait 1s before replacing "Mice" with "Hamsters"
-            "Dunk",
-            2000,
+            "Milton...",
+            4000, // wait 1s before replacing "Mice" with "Hamsters"
+            "Dunk...",
+            4000,
           ]}
           wrapper="span"
-          speed={10}
+          speed={5}
           // style={{ fontSize: "1em", display: "inline-block" }}
           style={{ fontWeight: "bold", color: "#D70040" }}
           repeat={Infinity}
         />
-        <a className="text-xl bg-gray-950 p-1 rounded-md block cursor-pointer flex justify-center text-gray-600">
+        <a className="text-xl bg-gray-950 p-1 rounded-md cursor-pointer flex justify-center text-gray-600">
           My Portfolio :)
         </a>
       </div>
